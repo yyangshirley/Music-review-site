23eae9c55e1b3a7170ae2932ddddefb1d5b20f7d
Yiyun Yang <55101147+yyangshirley@users.noreply.github.com>
2019-12-15 14:28:35 -0500

    Add functions for users to see details of songs/reviews ...

--- project-app/src/app/secure/secure.component.ts
+++ project-app/src/app/secure/secure.component.ts
@@ -1,3 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../home/shared/user.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '../User';
+import { Review } from '../Review';
+import { HttpService } from '../http.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Song } from '../Song';
+import { Playlist } from '../Playlist';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-secure',
+  templateUrl: './secure.component.html',
+  styleUrls: ['./secure.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+export class SecureComponent implements OnInit {
+  panelOpenState = false;
+
+  public loading=true;
+  public errorMsg:string;
+  public successMsg:string;
+  public username:string;
+  public email:string;
+  public password:string;
+  public user:User[];
+  public ratings: Review[];
+  public latestReview:Review[];
+  public songs: Song[];
+  public searchSong:Array<string>;
+
+  public allSong:Song[];
+  public reviews:Review[];
+  public detail:Song[];
+  public playlist:Playlist[];
+
+  public yourList:Playlist[];
+
+  public r:string;
+  public review_columns = ['songTitle', 'avgRating'];
+  public song_columns=['songTitle','songArtist','albumTitle','operation'];
+  
+  constructor(private httpService:HttpService,private userService:UserService, private router:Router,private activatedRoute:ActivatedRoute) { }
+  ngOnInit() {
+    this.getReview();
+    this.myplaylist();
+    this.getSong()
+  }
+
+  getReview(){
+    this.httpService.getReview().subscribe((reviews:Review[])=>{
+      this.ratings=reviews;
+
+      this.loading=false;
+    },(error:ErrorEvent)=>{
+      this.errorMsg=error.error.massage;
+      this.loading=false;
+    });  
+  }
+
+  getSong(){
+    this.httpService.getSong().subscribe((song:Song[])=>{
+      this.allSong=song;
+    })
+  }
+
+  // getLatest(title:string){
+  //   this.httpService.getLatestReview(title).subscribe((review:Review[])=>{
+  //     this.latestReview=review;
+  //     console.log(this.latestReview)
+  //   })
+  // }
+
+  add(){
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const username=p.username;
+      if(username){
+        document.getElementById("addSongs").style.display="";
+      }
+      else{
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+        this.errorMsg="Please sign in to add a new song!"
+      }
+    });
+
+  }
+
+  writeReview(){
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const user=p.username;
+      console.log(user)
+      if(user){
+        document.getElementById("writeReview").style.display="";
+      }
+      else{
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+        this.errorMsg="Please sign in to comment!"
+      }
+     
+    });
+  }
+
+  cancelReview(){
+    document.getElementById("writeReview").style.display="none";
+  }
   deleteSong(listTitle:string,song:string){
     this.successMsg="";
     this.errorMsg=""
@@ -46,6 +160,53 @@
         document.body.scrollTop = 0;
         this.errorMsg="Please sign in to add a new song!"
       }
+    });
+  }
+//get review
+  submit(songTitle:string,reviews:string, rating:number){
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const username=p.username;
+      var myDate = new Date();
+      var reviewDate=myDate.toLocaleDateString();
+      this.httpService.writeReview(username,songTitle,reviewDate,reviews,rating).subscribe((review:Review[])=>{
+        this.reviews=review;
+        console.log(this.reviews)
+      })
+    });
+    document.getElementById("writeReview").style.display="none";
+    this.getReview();
+    this.myplaylist();
+  }
+
+
+  search(keyword:string){
+    this.httpService.searchSong(keyword).subscribe((song:Song[])=>{
+      if(song.length==0){
+        this.errorMsg="There is no result!!! Please check the keyword."
+        document.getElementById("searching").style.display="none";
+
+      }
+      else{
+        this.errorMsg="";
+        document.getElementById("searching").style.display="";
+        this.songs=song;
+        
+      }
+    },(error:ErrorEvent)=>{
+      this.errorMsg=error.error.massage;
+    })
+  }
+ 
+  updatePlaylist(listTitle:string,description:string,status:string){
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const username=p.username;
+      this.httpService.updatePlaylist(listTitle,description,username,status).subscribe((playlist:Playlist[])=>{
+      })
+      console.log(status)
+      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0;
+      this.successMsg="Successfully modify your playlist!"
+      this.myplaylist();
     });
   }
 
@@ -121,3 +282,33 @@
     this.resetForm(form);
   }
 
+  resetForm(form: NgForm) {
+    form.resetForm();
+  }
+
+  logout(){
+    this.userService.deleteToken();
+    this.router.navigate(['/login']);
+  }
+
+  songDetail(song:string){
+    console.log(song);
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const keyword=p.username;
+      this.router.navigate(['/home/songDetail'],{
+        queryParams:{
+          songTitle:song,
+          username:keyword
+        }
+      });
+    });
+  }
+
+  navigate(username:string){
+    this.router.navigate(['/secure'],{
+      queryParams:{
+        username:username
+      }
+    });
+  }
+}
