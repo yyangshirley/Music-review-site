28aa79aa1d6ce1202a53ea52e430cdd7e5b0c4a7
Yiyun Yang <55101147+yyangshirley@users.noreply.github.com>
2019-12-15 14:21:41 -0500

    Export the interface for review

---
+++ project-app/src/app/Review.ts
@@ -0,0 +1,21 @@
+
+export interface Review{
+    _id:string;
+    songTitle:string;
+    averageRating:number;
+    userID:string;
+    reviews:string;
+    rating:number;
+    reviewDate:Date;
+    count:number;
+    average:number
+}
+
+/**
+ ** reviews:String,
+    rating:Number,
+    reviewDate:Date,
+    userID:String,
+    songTitle:String,
+    averageRating:Number
+ */
