b9d8426a0f0baa98d261cfdd92419e50639507e3
Yiyun Yang <55101147+yyangshirley@users.noreply.github.com>
2019-12-15 14:14:23 -0500

    Add the function to write reviews for this song.

--- project-app/src/app/home/song-detail/song-detail.component.ts
+++ project-app/src/app/home/song-detail/song-detail.component.ts
@@ -59,3 +59,54 @@
     });
   }
  
+  getReviews(title:string){
+    this.errorMsg="";
+
+      this.httpService.reviewDetail(title).subscribe((review:Review[])=>{
+        this.reviews=review;
+        console.log(this.reviews)
+      }),(error:ErrorEvent)=>{
+        this.errorMsg=error.error.massage;
+      }
+  }
+
+  writeReview(){
+    this.errorMsg="";
+
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const user=p.username;
+      console.log(user)
+      if(user){
+        document.getElementById("writeReview").style.display="";
+      }
+      else{
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+        this.errorMsg="Please sign in to comment!"
+      }
+     
+    });
+  }
+
+  //write the review
+  submit(reviews:string, rating:number){
+    this.successMsg="";
+
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const songTitle=p.songTitle;
+      const username=p.username;
+      var myDate = new Date();
+      var reviewDate=myDate.toLocaleDateString();
+      this.httpService.writeReview(username,songTitle,reviewDate,reviews,rating).subscribe((review:Review[])=>{
+        this.reviews=review;
+        console.log(this.reviews)
+      })
+      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0;
+      this.successMsg="Thank you for your review!!!"
+
+    });
+    document.getElementById("writeReview").style.display="none";
+    this.refresh();
+  }
+  
