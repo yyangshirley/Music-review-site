77fd44de19c1c7f4a5b5f1fcf3f5b713847e9417
Yiyun Yang <55101147+yyangshirley@users.noreply.github.com>
2019-12-15 14:25:57 -0500

    Add the function to add new songs to the site.

--- project-app/src/app/secure/secure.component.ts
+++ project-app/src/app/secure/secure.component.ts
@@ -18,3 +18,33 @@
   }
 
   
+  addSong(songTitle:string,songArtist:string,albumTitle:String,year:number,track:number,genre:number){
+    this.errorMsg=""
+    this.activatedRoute.queryParams.subscribe((p)=>{
+      const keyword=p.username;
+      console.log(songTitle)
+      if(keyword){
+        if(songTitle==""||songArtist==""){
+          this.errorMsg="Song title and artist are required!!!";
+        }
+        else{
+          this.errorMsg="";
+          this.httpService.createSong(songTitle,songArtist,albumTitle,year,track,genre).subscribe((song:Song[])=>{
+            this.detail=song;
+            console.log(song);
+            document.getElementById("addSongs").style.display="none";
+            document.getElementById("songDetails").style.display="";
+            this.successMsg="The song is created successfully!!!";
+            this.getSong()
+          }),(error:ErrorEvent)=>{
+            this.errorMsg=error.error.massage;
+          }
+        }
+      }
+      else{
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+        this.errorMsg="Please sign in to add a new song!"
+      }
+    });
+  }
