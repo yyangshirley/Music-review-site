acfa4415f67af0cad06a8b68d919b8647fcb5c81
Yiyun Yang <55101147+yyangshirley@users.noreply.github.com>
2019-12-15 14:06:53 -0500

    Add the function to set the privilege of users(site manager/normal)
    normal means normal users; site manager means having privilege as a site manager.

--- project-app/src/app/admin/admin.component.ts
+++ project-app/src/app/admin/admin.component.ts
@@ -1,3 +1,17 @@
+  setPrivilege(username:string){
+    this.httpService.setPrivilege(username,"sm").subscribe((user:User[])=>{
+      // this.users=user;
+    })
+    this.getAllUser()
+
+  }
+  cancelPri(username:string){
+    this.httpService.setPrivilege(username,"normal").subscribe((user:User[])=>{
+      // this.users=user;
+    })
+    this.getAllUser()
+  }
+
   setDe(username:string){
     this.httpService.setStatus(username,"deactivated").subscribe((user:User[])=>{
       // this.users=user;
